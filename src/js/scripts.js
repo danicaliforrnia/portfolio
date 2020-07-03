4f07fc7d330c4ce156df9854ab6adf0b2a67bd16
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-07-02 21:35:11 -0400

    refactor

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,32 +15,7 @@ $('.navbar-nav li a').on('click', () => {
     }
 });
 
-/* Card Slider - Swiper */
-new Swiper('.card-slider', {
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 3,
-    spaceBetween: 20,
-    breakpoints: {
-        // when window is <= 992px
-        992: {
-            slidesPerView: 2,
-        },
-        // when window is <= 768px
-        768: {
-            slidesPerView: 1,
-        },
-    },
-});
 
-/* Image Slider - Swiper */
 new Swiper('.image-slider', {
     autoplay: {
         delay: 2000,
@@ -54,27 +29,22 @@ new Swiper('.image-slider', {
     spaceBetween: 30,
     slidesPerView: 5,
     breakpoints: {
-        // when window is <= 380px
         380: {
             slidesPerView: 1,
             spaceBetween: 10,
         },
-        // when window is <= 516px
         516: {
             slidesPerView: 2,
             spaceBetween: 10,
         },
-        // when window is <= 768px
         768: {
             slidesPerView: 3,
             spaceBetween: 20,
         },
-        // when window is <= 992px
         992: {
             slidesPerView: 4,
             spaceBetween: 30,
         },
-        // when window is <= 1200px
         1200: {
             slidesPerView: 5,
             spaceBetween: 30,
@@ -103,7 +73,6 @@ $(window).scroll(() => {
                         },
                         complete() {
                             $this.text(this.countNum);
-                            // alert('finished');
                         },
                     });
             });

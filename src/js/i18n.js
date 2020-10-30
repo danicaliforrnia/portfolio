9d7e9b7370ef10569e24b371b4a4dd298043a550
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-10-30 17:04:41 -0400

    perf(translate): Improving translation code
    iterating json file elements to replace each value in html.

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -1,47 +1,10 @@
 import $ from 'jquery';
 
 function changeText(resources) {
-    $('#languages').text(resources.languages);
-    $('#en').text(resources.en);
-    $('#es').text(resources.es);
-    $('#home-nav').text(resources.home);
-    $('#about-me-nav').text(resources.aboutMe);
-    $('#services-nav').text(resources.services);
-    $('#projects-nav').text(resources.projects);
-    $('.contact-me-nav').text(resources.contactMe);
-    $('#greeting').text(resources.greeting);
-    $('#header-text').text(resources.headerText);
-    $('#know-me').text(resources.knowMe);
-    $('#know-me-text').text(resources.knowMeText);
-    $('#check-cv').text(resources.checkCV);
-    $('#requirement-engineering').text(resources.requirementEngineering);
-    $('#requirement-engineering-text').text(resources.requirementEngineeringText);
-    $('#app-development').text(resources.appDevelopment);
-    $('#app-development-text').text(resources.appDevelopmentText);
-    $('#app-deployment').text(resources.appDeployment);
-    $('#app-deployment-text').text(resources.appDeploymentText);
-    $('#ats-text').text(resources.atsText);
-    $('#netcom-text').text(resources.netcomText);
-    $('.tecnologies-title').text(resources.technologiesTitle);
-    $('#clients-counter').text(resources.clientsCounter);
-    $('#projects-counter').text(resources.projectsCounter);
-    $('#tech-counter').text(resources.techCounter);
-    $('#coffee-counter').text(resources.coffeeCounter);
-    $('#contact-me-text').text(resources.contactMeText);
-    $('#name-label').text(resources.nameLabel);
-    $('#email-label').text(resources.emailLabel);
-    $('#message-label').text(resources.messageLabel);
-    $('#checkbox-label').text(resources.checkboxLabel);
-    $('.privacy').text(resources.privacy);
-    $('.terms').text(resources.terms);
-    $('#submit').text(resources.submit);
-    $('#slogan').text(resources.slogan);
-    $('#links').text(resources.links);
-    $('#my-cv').text(resources.myCV);
-    $('#read').text(resources.read);
-    $('.read-connector').text(resources.readConnector);
-    $('#social-networks').text(resources.social);
-    $('#title-name').text(resources.titleName);
+    for (const e in resources) {
+        $(`#${e}`).text(resources[e]);
+        $(`.${e}`).text(resources[e]);
+    }
 }
 
 function getResources(lang) {

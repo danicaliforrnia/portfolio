8594af6baa5f39fb22d5109ce9bea5ac7944323b
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2026-02-14 10:48:33 +0100

    ci: deploy to GitHub Pages
    Signed-off-by: Daniel Stefanelli <daniel.stefanelli.h@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    output: 'export',
+    basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
